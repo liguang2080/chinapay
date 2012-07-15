b801e196e17cf75de4da2a7ecfd1e6645f2b1cc2
lg2046 <lg2046@gmail.com>
2012-07-15 14:17:57 +0800

    bump to 0.4.0

--- lib/chinapay/config.rb
+++ lib/chinapay/config.rb
@@ -1,21 +1,25 @@
 module Chinapay
+  
+  class << self; attr_accessor :config; end
+  
   # 配置采用统一的 parter_id key seller
   def self.load_config
     begin
       # filename = "#{Rails.root}/config/chinapay.yml"
       filename = File.expand_path(File.dirname(__FILE__) + "/../chinapay.yml")
-      CONFIG = YAML.load(File.open(filename))
+      @config = YAML.load(File.open(filename))[Rails.env]
     rescue Exception => e
       puts <<-EXAMPLE
       ---
-      tencent:
-        parter: 121111XXXX
-        key: XXXXXXXXXXXXXXXXXX
-        seller: 121111XXXX
-      alipay:
-        parter: 12111XXXX
-        key: XXXXXXX
-        seller: XXXXX@XXXXX.com
+      development:
+        tencent:
+          parter: 121111XXXX
+          key: XXXXXXXXXXXXXXXXXX
+          seller: 121111XXXX
+        alipay:
+          parter: 12111XXXX
+          key: XXXXXXX
+          seller: XXXXX@XXXXX.com
       EXAMPLE
       raise "Please configure your Chinapay settings in #{filename}."
     end
