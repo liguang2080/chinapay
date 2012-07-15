f4bc17b63359a7a4b639de7275d9fc64491e4d93
lg2046 <lg2046@gmail.com>
2012-07-15 12:57:25 +0800

    build request

--- lib/chinapay.rb
+++ lib/chinapay.rb
@@ -9,8 +9,17 @@ require File.dirname(__FILE__) + '/chinapay/response/tencent'
 require File.dirname(__FILE__) + '/chinapay/response/alipay'
 
 module Chinapay
+  
+  ADAPTER_TYPES = ["alipay", "tenpay", "1002", "1038", "1003", "1052", "1005", "1020", "1024", "1022", "1006", "1008", "1004", "1021", "1009", "1025", "1027", "1032"]
+  
   def self.build_request(adapter, title, total_fee, notify_url, attach, bank_type = '0')
-    "Chinapay::Request::#{adapter.to_s.classify}".safe_constantize.new(title, total_fee, notify_url, attach, bank_type)
+    adapter = adapter.to_s
+    
+    raise "Not vaid chinapay adapter, it should be in #{ADAPTER_TYPES}" unless ADAPTER_TYPES.include?(adapter)
+    
+    return Chinapay::Request::Alipay.new(title, total_fee, notify_url, attach, nil) if adapter == "alipay"
+    return Chinapay::Request::Tencent.new(title, total_fee, notify_url, attach, '0') if adapter == "tenpay"
+    Chinapay::Request::Tencent.new(title, total_fee, notify_url, attach, adapter)
   end
 
   def self.build_response(params)
